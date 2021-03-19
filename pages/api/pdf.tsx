d748411dbedab18bd958680742a20a680b037dfa
Jonny (PC) <jonathanaingall@gmail.com>
2021-03-19 18:21:52 +0000

    Add letterhead

--- pages/api/pdf.tsx
+++ pages/api/pdf.tsx
@@ -3,19 +3,37 @@ import QRCode from "qrcode";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 interface PdfRequest extends NextApiRequest {
-  query: {
+  body: {
     url: string;
+    name: string;
+    street: string;
+    area: string;
+    city: string;
+    postcode: string;
+    email: string;
   };
 }
 export default async (req: PdfRequest, res: NextApiResponse) => {
-  const code = await QRCode.toDataURL(req.query.url);
-  const doc = new PDFDocument({ size: "A4" });
-  doc.pipe(res);
-  doc.image(code, doc.page.width - 85, doc.page.height - 85, {
-    width: 75,
-    align: "right",
-    valign: "bottom",
+  const { name, street, area, city, postcode, email } = req.body;
+  const letterhead = [name, street, area, city, postcode].filter((val) => val);
+  if (email) {
+    letterhead.push("", email);
+  }
+  const code = await QRCode.toDataURL(req.body.url);
+  const doc = new PDFDocument({
+    size: "A4",
+    margin: 20,
   });
+  doc.pipe(res);
+  doc
+    .text(letterhead.join("\n"), 10, 40, {
+      align: "right",
+    })
+    .image(code, doc.page.width - 85, doc.page.height - 85, {
+      width: 75,
+      align: "right",
+      valign: "bottom",
+    });
   res.status(200);
   doc.end();
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import Head from "next/head";
-import dynamic from "next/dynamic";
 import { useState } from "react";
 
+const defaultLetterhead = `Joe Bloggs,
+123 Imaginary Lane,
+Imaginary Area,
+Imagination City,
+AB1 2CD
+
+mail@example.com`;
+
+interface InputProps {
+  name: string;
+  placeholder: string;
+  required?: boolean;
+}
+const Input = ({ name, placeholder, required = false }: InputProps) => (
+  <input
+    placeholder={placeholder}
+    type="text"
+    name={name}
+    id={name}
+    className="block border border-blue-700 rounded p-2 w-full"
+    required={required}
+  />
+);
+
 export default function Home() {
-  const [url, setUrl] = useState<string>();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -16,25 +38,34 @@ export default function Home() {
           Letter with QR Code PDF Generator
         </h1>
 
-        <p className="mt-3 text-2xl">Get started by entering a URL below</p>
+        <p className="mt-3 text-2xl">
+          Get started by entering your details and QR Code URL below
+        </p>
 
-        <div className="flex items-center mt-3">
-          <input
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            type="text"
-            name="url"
-            id="url"
-            className="block border border-blue-700 rounded rounded-r-none p-2 w-64"
-          />
-          <a
-            className="block bg-blue-700 text-white p-2 border rounded rounded-l-none border-blue-700"
-            target="_blank"
-            href={`/api/pdf?url=${encodeURIComponent(url)}`}
+        <form
+          target="_blank"
+          className="w-72 space-y-2 mt-3"
+          action="/api/pdf"
+          method="POST"
+        >
+          <Input placeholder="Joe Bloggs" name="name" />
+          <Input placeholder="123 Imaginary Lane" name="street" />
+          <Input placeholder="Imaginary Area" name="area" />
+          <Input placeholder="City" name="city" />
+          <Input placeholder="AB1 2CD" name="postcode" />
+          <Input placeholder="mail@example.com" name="email" />
+          <Input placeholder="https://google.com" name="url" required />
+          <p className="my-1">We do not store your data!</p>
+          <p className="my-1">
+            However, you can generate only a QR Code by filling in only the URL
+          </p>
+          <button
+            className="block w-full bg-blue-700 text-white p-2 border rounded border-blue-700"
+            type="submit"
           >
             Generate
-          </a>
-        </div>
+          </button>
+        </form>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
