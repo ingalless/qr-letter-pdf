032d895baa0835a8d12a5b849f3d350312378b70
Jonathan <jonny@mondago.com>
2021-03-20 08:19:11 +0000

    Add cloudflare analytics

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "4f2ff9e6e3a34d1c88f4c9d463fa9ea7"}'
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
