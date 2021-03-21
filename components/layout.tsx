ee576809dd0e6c256ae5756d39bf1a5bc66ee86f
ingalless <jonathanaingall@gmail.com>
2021-03-21 15:59:02 +0000

    Remove vercel footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,16 +19,8 @@ const Layout: React.FC<LayoutProps> = ({
       <h1 className="text-3xl font-bold">{title}</h1>
       {children}
     </main>
-    <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
-      <a
-        className="flex items-center justify-center"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-      </a>
+    <footer className="flex text-center items-center justify-center w-full h-24 border-t text-xs font-semibold tracking-tight text-gray-700">
+      {title}
     </footer>
   </div>
 );
