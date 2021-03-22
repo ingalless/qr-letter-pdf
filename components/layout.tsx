e917bdd90afe2807257c4b16de07617577424016
Jonny (PC) <jonathanaingall@gmail.com>
2021-03-22 21:12:40 +0000

    Link

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,8 +19,14 @@ const Layout: React.FC<LayoutProps> = ({
       <h1 className="text-3xl font-bold">{title}</h1>
       {children}
     </main>
-    <footer className="flex text-center items-center justify-center w-full h-24 border-t text-xs font-semibold tracking-tight text-gray-700">
+    <footer className="flex flex-col text-center items-center justify-center w-full h-24 border-t text-xs font-semibold tracking-tight text-gray-700">
       {title}
+      <pre className="block bg-gray-50 border border-gray-400 rounded-sm font-mono font-small px-1 mt-1">
+        &lt;/&gt; by{" "}
+        <a className="text-blue-700 underline" href="https://ingalless.com">
+          Jonny
+        </a>
+      </pre>
     </footer>
   </div>
 );
